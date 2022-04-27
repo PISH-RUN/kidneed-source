dcf3f6ca2b5dd97cac8da713b14eb0ce8790b6d4
ali shabani <alishabani9270@gmail.com>
2022-04-27 10:40:31 +0430

    fix: fixed remove entity

--- src/bootstrap/remove-duplicate-entity.js
+++ src/bootstrap/remove-duplicate-entity.js
@@ -1,7 +1,19 @@
 async function removeDuplicateEntity(strapi) {
-  await strapi
-    .query("api::entity.entity")
-    .deleteMany({ where: { content: null } });
+  const eQuery = strapi.query("api::entity.entity");
+
+  const entities = await eQuery.findMany({
+    where: { content: null },
+    select: ["id"],
+  });
+
+  if (entities.length === 0) {
+    return;
+  }
+
+  console.log({ entities });
+  const ids = entities.map((e) => e.id);
+
+  await eQuery.deleteMany({ where: { id: { $in: ids } } });
 }
 
 module.exports = removeDuplicateEntity;
