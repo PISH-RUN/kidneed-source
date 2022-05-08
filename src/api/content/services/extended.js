2d3f03f71b8d0b91faa66ff0f0440cac4e0d20b6
ali shabani <alishabani9270@gmail.com>
2022-05-08 11:14:46 +0430

    fix: added type for syncing content and entity

--- src/api/content/services/extended.js
+++ src/api/content/services/extended.js
@@ -6,7 +6,7 @@ module.exports = ({ strapi }) => ({
   async createEntity(content) {
     await strapi.service("api::entity.entity").create({
       data: {
-        ...pick(content, ["title", "description", "meta", "sourceUrl"]),
+        ...pick(content, ["title", "description", "meta", "sourceUrl", "type"]),
         minAge: content.ageCategory || 0,
         maxAge: content.maxAge || 11,
         gender: content.suitableFor || "both",
